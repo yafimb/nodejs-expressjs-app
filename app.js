cb3fa00a7eb5d30bbcde93ea59dd18bbc9dcdfe8
ZENBOOK\fimab <yafimb@zoara.com>
2019-04-04 16:08:47 +0300

    handlong routes

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const http      = require('http');
 const express   = require('express');
 const app       = express();
 
-app.use((req, res, next)=>{
-    console.log('MID1');
-    next();//allow to continue to the next middleware
+app.use('/',(req, res, next)=>{
+    console.log('This is allways run')
+    next();
 });
 
-app.use((req, res, next)=>{
-    console.log('MID2');
+app.use('/add-product',(req, res, next)=>{
+    res.send('<html><h1>add product</h1></html>');
+});
+
+app.use('/',(req, res, next)=>{
     res.send('<html><h1>Hello from express.</h1></html>');
 });
 
-const server    = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
