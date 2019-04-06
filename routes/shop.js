acc8b9092b64b7a8925b4e575cd179cf2b320590
ZENBOOK\fimab <yafimb@zoara.com>
2019-04-06 14:57:57 +0300

    pug templates

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const router    = express.Router();
 router.get('/',(req, res, next) => {
     console.log(adminData.products);
     //res.sendFile(path.join(rootDir,'views', 'shop.html'));
-    res.render('shop');
+    res.render('shop', {prods: adminData.products, docTitle: 'Shop'});
 });
 
 module.exports = router
