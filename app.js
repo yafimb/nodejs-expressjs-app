1b1773801205787b4e58135c9705641839926061
ZENBOOK\fimab <yafimb@zoara.com>
2019-04-06 11:26:46 +0300

    serve static files

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const http      = require('http');
-const express   = require('express');
-const app       = express();
+const path        = require('path');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes  = require('./routes/shop');
 
-app.use('/',(req, res, next)=>{
-    console.log('This is allways run')
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/add-product',(req, res, next)=>{
-    res.send('<html><h1>add product</h1></html>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next)=>{
-    res.send('<html><h1>Hello from express.</h1></html>');
+app.use((req, res, next)=> {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
